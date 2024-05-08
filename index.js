a483b81ac0731e946fecc150de685ccc4247e8b1
the-okapi <156860750+the-okapi@users.noreply.github.com>
2024-05-08 11:27:05 -0700

    Updating

--- index.js
+++ index.js
@@ -7,10 +7,10 @@ function get(url) {
   client.send();
 }
 
-document.getElementById('overall').innerHTML = get('data/overall.txt');
-document.getElementById('mud').innerHTML = get('data/mud.txt');
-document.getElementById('water').innerHTML = get('data/water.txt');
-document.getElementById('sand').innerHTML = get('data/sand.txt');
-document.getElementById('art').innerHTML = get('data/art.txt');
-document.getElementById('science').innerHTML = get('data/science.txt');
-document.getElementById('outdoorclassroom').innerHTML = get('data/outdoorclassroom.txt'); 
+document.getElementById('overall').innerHTML = get('assets/data/overall.txt');
+document.getElementById('mud').innerHTML = get('assets/data/mud.txt');
+document.getElementById('water').innerHTML = get('assets/data/water.txt');
+document.getElementById('sand').innerHTML = get('assets/data/sand.txt');
+document.getElementById('art').innerHTML = get('assets/data/art.txt');
+document.getElementById('science').innerHTML = get('assets/data/science.txt');
+document.getElementById('outdoorclassroom').innerHTML = get('assets/data/outdoorclassroom.txt'); 
