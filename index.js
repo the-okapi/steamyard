b7eeb740b67ae223d5ad7c2bee765086c461cf70
the-okapi <156860750+the-okapi@users.noreply.github.com>
2024-05-08 11:22:13 -0700

    Updating

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
-var client = new XMLHttpRequest();
-client.open('GET', 'data/test.txt');
-client.onload = function() {
-  alert(client.responseText);
+function get(url) {  
+  let client = new XMLHttpRequest();
+  client.open('GET', url);
+  client.onload = function() {
+    return this.responseText;
+  }
+  client.send();
 }
-client.send();
+
+document.getElementById('overall').innerHTML = get('data/overall.txt');
+document.getElementById('mud').innerHTML = get('data/mud.txt');
+document.getElementById('water').innerHTML = get('data/water.txt');
+document.getElementById('sand').innerHTML = get('data/sand.txt');
+document.getElementById('art').innerHTML = get('data/art.txt');
+document.getElementById('science').innerHTML = get('data/science.txt');
+document.getElementById('outdoorclassroom').innerHTML = get('data/outdoorclassroom.txt'); 
